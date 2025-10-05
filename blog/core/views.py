e94c845d4922cc00fc48d4acb94e34e4e45225cc
Sanjitha Manivelan <sanjitha@unknownb6c324f827ca.attlocal.net>
2025-10-05 16:32:14 -0700

    6

---
+++ blog/core/views.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, Blueprint
+from blog.models import BlogPost
+
+core = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html', blog_posts=blog_posts)
